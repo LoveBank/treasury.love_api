8bfa9e0438b75b2dd6596103d9dc46dc0bbc461f
Jared L Jennings <jared@jaredjennings.org>
2017-05-28 22:44:51 -0500

    Fixed devise_parameter_sanitizer Added nickname and name to allowed parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
   include ActionController::Serialization
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    Rails.logger.info "in ApplicationController.rb configure_permitted_parameters!"
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :nickname])
+  end
+
 end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe 'Authentication', type: :request, focus: :true do
 
   context 'user signed in' do
     describe 'GET #current' do
-      it 'Should respond with status 200(OK)' do
-
+      before do
         @user = FactoryGirl.create :user
+      end
+
+      it 'Should respond with status 200(OK)' do
         # we are not using confirmation right now
         # @user.confirm
 
@@ -15,12 +17,18 @@ RSpec.describe 'Authentication', type: :request, focus: :true do
         get '/entries', params: {}, headers: @auth_headers
         expect(response).to have_http_status(:success)
       end
+
+      #TODO: add this test once we figure out why this route doesn't work.
+      # it 'Should respond with user details' do
+      #   @auth_headers = @user.create_new_auth_token
+      #   get '/auth/edit', params: {}, headers: @auth_headers
+      # end
     end
   end
 
   describe 'POST /auth (Sign Up process)' do
     it 'Should respond with status 200(OK)' do
-      post user_registration_path(:name => 'Test User', :email => 'email@email.com', :password => 'qwertyuiop', :confirmation_password => 'qwertyuiop')
+      post user_registration_path(:name => 'Test User', :email => 'email@email.com', :password => 'qwertyuiop')
       expect(response).to have_http_status(200)
     end
 
@@ -29,6 +37,27 @@ RSpec.describe 'Authentication', type: :request, focus: :true do
         post user_registration_path(:email => 'email@email.com', :password => 'qwertyuiop')
       }.to change(User, :count).by(1)
     end
+
+    it 'Should respond with status success' do
+      post user_registration_path(:name => 'Test User', :email => 'email@email.com', :password => 'qwertyuiop')
+      expect(response).to have_http_status(200)
+    end
+
+    it 'Should respond with name)' do
+      post user_registration_path(:name => 'Test User', :email => 'email@email.com', :password => 'qwertyuiop')
+      expect(json['data']['name']).to match('Test User')
+    end
+
+    it 'Should respond with nickname)' do
+      post user_registration_path(:nickname => 'Test User', :email => 'email@email.com', :password => 'qwertyuiop')
+      expect(json['data']['nickname']).to match('Test User')
+    end
+
+    it 'Should respond with email)' do
+      post user_registration_path(:name => 'Test User', :email => 'email@email.com', :password => 'qwertyuiop')
+      expect(json['data']['email']).to match('email@email.com')
+    end
+
   end
 
   # describe 'POST /auth/confirmation (Confirmation process)' do
